77fcc81df4adcb39e928ac22c1d8a899df4bde27
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-09 22:51:20 +0530

    complete accordion  project

--- Accordion/src/App.js
+++ Accordion/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import data from './data';
-import SingleQuestion from './Question';
+//import SingleQuestion from './Question';
+import Question from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question,setQuestion]= useState(data)  //returns an array
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {question.map((question) => {
+            return <Question key={ question.id} {...question} />
+        })}   
+
+          
+        </section>
+      </div>
+</main>
+  )
 }
 
 export default App;
